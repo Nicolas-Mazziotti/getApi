2d1e09ff727ac9d530828669ee9d0b6ebad4f07a
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-09-15 20:56:26 -0300

    Create function to fetch API

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const inputSearch = document.querySelector('#inputSearch');
 const btnSearch = document.querySelector('#btnSearch');
+
+window.addEventListener('DOMContentLoaded', () => {
+    getApi()
+})
+
+//Funcion para traer api de Rick and Morty usando fetch
+
+const getApi = async () => {
+    try {
+        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const data = await response.json()
+        console.log(data)
+    } catch (err) {
+        console.error(err)
+    }
+ 
+}
