765a732ec2d53f44ea2be8a03ab772fd70e534b7
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-09-16 16:34:11 -0300

    Function to showCharacters in the DOM

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputSearch = document.querySelector('#inputSearch');
 const btnSearch = document.querySelector('#btnSearch');
+const charactersContainer = document.getElementById('charactersContainer')
+
 
 window.addEventListener('DOMContentLoaded', () => {
     getApi()
@@ -12,8 +14,26 @@ const getApi = async () => {
         const response = await fetch('https://rickandmortyapi.com/api/character')
         const data = await response.json()
         console.log(data)
+        showCharacters(data)
     } catch (err) {
         console.error(err)
     }
- 
+}
+
+const showCharacters = (dataCharacters) => {
+    
+    const arrCharacters = dataCharacters.results
+    console.log(arrCharacters)
+    arrCharacters.map((char)=> {
+        const targetContainer = document.createElement('div')
+        const titleElement = document.createElement('h3')
+        const imgElement = document.createElement('img')
+        imgElement.setAttribute('src', `${char.image}`)
+        const titleName = document.createTextNode(`${char.name}`)
+
+        charactersContainer.appendChild(targetContainer)
+        targetContainer.append(titleElement)
+        titleElement.append(titleName)
+        targetContainer.append(imgElement)
+    }) 
 }
