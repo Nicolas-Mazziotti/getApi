f5cab42b718e38e8366ce376497357f437db8a4f
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-10-22 11:37:04 -0300

    Function search characters

--- script.js
+++ script.js
@@ -1,118 +1,30 @@
-// const inputSearch = document.querySelector('#inputSearch');
-// const btnSearch = document.querySelector('#btnSearch');
-// const charactersContainer = document.getElementById('charactersContainer');
-// const btnNext = document.querySelector('#nextPage');
-// const btnPrevious = document.querySelector('#previousPage');
-
-// let characters = []
-
-// window.addEventListener('DOMContentLoaded', () => {
-//         getApi()
-// })
-
-// //Funcion para traer api de Rick and Morty usando fetch
-
-// const getApi = async () => {
-//     try {
-//         const response = await fetch(`https://rickandmortyapi.com/api/character`)
-//         const data = await response.json()
-//         console.log(data)
-//         showCharacters(data)
-//         charactersPage(data)
-//         handleInput(data)
-    
-        
-        
-        
-//     } catch (err) {
-//         console.error(err)
-//     }
-    
-// }
-
-// const showCharacters = (dataCharacters) => {
-    
-//     const arrCharacters = dataCharacters.results
-//     console.log(arrCharacters)
-//     arrCharacters.map((char)=> {
-        
-//         const targetContainer = document.createElement('div')
-//         targetContainer.classList.add('cardContainer')
-//         const imgElement = document.createElement('img')
-//         imgElement.setAttribute('src', `${char.image}`)
-//         imgElement.classList.add('card-img-top')
-//         const titleElement = document.createElement('h3')
-//         const titleName = document.createTextNode(`${char.name}`)
-        
-//         charactersContainer.appendChild(targetContainer)
-//         targetContainer.appendChild(imgElement)
-//         targetContainer.appendChild(titleElement)
-//         titleElement.appendChild(titleName)
-
-        
-
-//     })
-    
-// }
-
-
-// //Function pagination
-
-
-// const charactersPage = (dataCharacters) => {
-//     const nexting = dataCharacters.info.next 
-//     btnNext.addEventListener('click', () => {
-
-//         getApi(nexting)
-//         console.log(nexting)
-//         })
-//     }  
-    
-
-// //Function search characters
-
-// const handleInput = (arrCharacters) => {
-//     console.log(arrCharacters)
-//     let inputValue= inputSearch.value
-//     console.log(inputValue)
-//     let nameCharacter = arrCharacters.results
-    
-//     //filter name
-//     const newCharacters = nameCharacter.filter((nameChar) => {
-//         nameChar.name.includes(inputValue)
-//         showCharacters(newCharacters)
-//     })
-
-// }
-
-
-// btnSearch.addEventListener('click', () => {
-//         handleInput()
-//     })
-
-
 
 const inputSearch = document.querySelector('#inputSearch')
+const tagName = document.getElementsByClassName('cardContainer')
+console.log(tagName)
+
 
 window.addEventListener('DOMContentLoaded', async() => {
-    console.log('Dom Loaded')
-    const arrCharacters = await getCharacters()
-    showCharacters(arrCharacters)
+    console.log('Dom Loaded');
+    const arrCharacters = await getCharacters();
+    showCharacters (arrCharacters);
+    getValues()
+    
 
 })
 
 
-// Funcion para traer api Rick and Morty
+// Function to get rick and morty api
 
 const getCharacters = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/character')
     const data = await response.json()
     const dataArr = data.results
-    return dataArr
-}
+     return dataArr
+ }
 
 //Function to show characters at dom
-//(characters) recibe array line 99/100
+//(characters) receive array line 99/100
 const showCharacters = (characters) =>{
     characters.map((char) => {
         const targetContainer = document.createElement('div')
@@ -129,6 +41,39 @@ const showCharacters = (characters) =>{
         titleElement.appendChild(titleName)
     })
 }
+
+// Function to search characters
+
+const getValues = () => {
+    let er = new RegExp(inputSearch.value, "i")
+    for (let i=0; i<tagName.length; i++) {
+        let valueTagName = (tagName[i])
+        if(er.test(valueTagName.innerText)){
+            valueTagName.classList.remove('hide')
+        }else{
+            valueTagName.classList.add('hide')
+        }
+    }
+    
+    
+    
+    
+    
+//     console.log(users)
+//    const searchCharacters = users.filter((user) => {
+//         let arrNames = user.name
+//         if(arrNames === inputSearch.value){
+//             showCharacters(searchCharacters)
+//         }        
+// })
+}
+
+ inputSearch.addEventListener('keyup', getValues);
+
+
+
+
+
 
 
 
